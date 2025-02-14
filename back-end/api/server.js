bed93c93566b34c0eed61608a1a856c50941c128
Dayvison Lima <limaare@outlook.com>
2025-02-14 16:27:34 -0300

    ajustes para o deploy

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -1,6 +1,9 @@
 import express from 'express'
 import cors from 'cors'
 import { database } from './connect.js'
+import path from 'path'
+
+const __dirname = path.resolve()
 
 const app = express()
 
@@ -9,16 +12,32 @@ const PORT = 3001
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (request, response) => {
+app.get('/api/', (request, response) => {
     response.send('Olá, Mundo!!!')
 })
 
-app.get('/artists', async (request, response) => {
-    response.send(await database.collection('artists').find({}).toArray())
-})
+app.get('/api/artists', async (request, response) => {
+    try {
+        const artists = await database.collection('artists').find({}).toArray();
+        response.send(artists);
+    } catch (error) {
+        response.status(500).send({ message: "Erro ao buscar artistas", error });
+    }
+});
 
-app.get('/songs', async (request, response) => {
-    response.send(await database.collection('songs').find({}).toArray())
+app.get('/api/songs', async (request, response) => {
+    try {
+        const songs = await database.collection('songs').find({}).toArray();
+        response.send(songs);
+    } catch (error) {
+        response.status(500).send({ message: "Erro ao buscar músicas", error });
+    }
+});
+
+app.use(express.static(path.join(__dirname, '../front-end/dist')))
+
+app.get('*', async (request, response) => {
+    response.sendFile(path.join(__dirname, '../front-end/dist/index.html'))
 })
 
 app.listen(PORT, () => {
--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,6 +1,9 @@
+import 'dotenv/config'
 import axios from 'axios'
 
-const URL = "http://localhost:3001"
+const NODE_ENV = import.meta.env.VITE_NODE_ENV
+
+const URL = NODE_ENV === 'development' ? 'http://localhost:3001/api' : '/api'
 
 const responseArtist = await axios.get(`${URL}/artists`)
 const responseSongs = await axios.get(`${URL}/songs`)
