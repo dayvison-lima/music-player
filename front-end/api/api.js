54fe887ad878ff63e3d320579d280eec3fabce7e
Dayvison Lima <limaare@outlook.com>
2025-02-14 16:44:56 -0300

    removendo variável de ambiente

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,9 +1,10 @@
-import 'dotenv/config'
+// import 'dotenv/config'
 import axios from 'axios'
 
-const NODE_ENV = import.meta.env.VITE_NODE_ENV
+// const NODE_ENV = import.meta.env.VITE_NODE_ENV
 
-const URL = NODE_ENV === 'development' ? 'http://localhost:3001/api' : '/api'
+// const URL = 'http://localhost:3001/api'
+const URL = 'https://music-player-th45.onrender.com/api'
 
 const responseArtist = await axios.get(`${URL}/artists`)
 const responseSongs = await axios.get(`${URL}/songs`)
