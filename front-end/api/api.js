e9fb7b19a734c96dd54dfb05decf712706c0127a
Dayvison Lima <limaare@outlook.com>
2025-02-16 20:35:21 -0300

    corrigindo a url

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 // const NODE_ENV = import.meta.env.VITE_NODE_ENV
 
 // const URL = 'http://localhost:3001/api'
-const URL = 'https://music-player-th45.onrender.com/api'
+const URL = 'https://music-player-1-22sr.onrender.com/api'
 
 const responseArtist = await axios.get(`${URL}/artists`)
 const responseSongs = await axios.get(`${URL}/songs`)
